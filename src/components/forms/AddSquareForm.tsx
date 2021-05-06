a99adc865ed2659a3a53783b639e39b61f3be177
krowter <krishna.rowter@gmail.com>
2021-05-06 11:25:40 +0700

    add color validation pattern checking

--- src/components/forms/AddSquareForm.tsx
+++ src/components/forms/AddSquareForm.tsx
@@ -1,4 +1,6 @@
-import React, { FormEvent } from "react";
+import React, { FormEvent, useState } from "react";
+
+import { validateColorInput } from "../../helpers";
 
 import { Color } from "types";
 
@@ -13,21 +15,43 @@ const areEqualBetweenRenders = (
 
 export const AddSquareForm: React.FC<AddSquareFormProps> = React.memo(
   ({ addSquare }) => {
+    const [isInputValid, setIsInputValid] = useState(false);
+
     const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
       // prevent page reload upon form submit
       e.preventDefault();
 
-      const form = new FormData(e.target as HTMLFormElement);
+      const form = new FormData(e.currentTarget);
       const color = form.get("color");
 
       addSquare(color as string);
     };
 
+    const handleInputChange = (e: FormEvent<HTMLInputElement>) => {
+      const color = e.currentTarget.value;
+      const isValid = Boolean(color) && validateColorInput(color as string);
+
+      setIsInputValid(isValid);
+    };
+
     return (
       <form className="add-square-form" onSubmit={handleFormSubmit}>
         <label htmlFor="new-color-input">Add new color</label>
-        <input id="new-color-input" name="color" />
-        <button type="submit">Add</button>
+        {/* 7 is color in hex format + "#" at the beginning */}
+        <input
+          id="new-color-input"
+          maxLength={7}
+          name="color"
+          onChange={handleInputChange}
+        />
+        <button disabled={!isInputValid} type="submit">
+          Add
+        </button>
+        {!isInputValid && (
+          <span className="warning-text">
+            Please enter a valid hex color value (begins with #)
+          </span>
+        )}
       </form>
     );
   },
--- src/helpers.ts
+++ src/helpers.ts
@@ -1,1 +1,11 @@
 export const createRandomId = (): string => Math.random().toString();
+
+export const validateColorInput = (color: string) => {
+  // valid hex pattern
+  // - begins with "#"
+  // - has 6 or 3 (in the case of shorthand form) characters
+  // - use hexadecimal values 0 to 9 and a to f
+  const pattern = /^#([0-9A-F]{3}){1,2}$/i;
+
+  return pattern.test(color);
+};
